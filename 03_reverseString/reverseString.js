ebd0f83dab972005a34a720bf393b865b3eead46
Tonse Shenoy <mamtasmailbox@gmail.com>
2021-11-05 14:04:35 -0400

    exercises

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,8 @@
-const reverseString = function(str) {
-
+const reverseString = function (str) {
+  const myArray = str.split("");
+  console.log(myArray);
+  return myArray.reverse().join("");
 };
-
+reverseString("hello");
 // Do not edit below this line
 module.exports = reverseString;
